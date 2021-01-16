1a2af3ae03c8fbd244a4e6a389142e2be13582ba
Viviño <Vivinh0@users.noreply.github.com>
2021-01-16 11:44:48 +0100

    Add /api/exercise/new-user test

---
+++ tests/user.test.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const userModel = require("../models/userModel");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe("Test URL Exercise Tracker Microservice", () => {
+  let server;
+
+  // Start server before each test
+  beforeEach((done) => {
+    server = require("../server");
+    done();
+  });
+  // Stop server after each test
+  afterEach((done) => server.close(() => done()));
+  // Clean user collection and close connection with MongoDB
+  after(async () => {
+    await userModel.deleteMany();
+    await mongoose.disconnect();
+  });
+
+  describe("POST /api/exercise/new-user", () => {
+    it("should return a JSON response with keys '_id' and 'username'.", (done) => {
+      chai
+        .request(server)
+        .post("/api/exercise/new-user")
+        .send({ username: "John Doe" })
+        .end((err, res) => {
+          // Get results
+          const actualResult = res.body;
+
+          // Test results
+          expect(actualResult)
+            .to.be.an("object")
+            .that.has.all.keys("_id", "username");
+
+          expect(actualResult.username).to.be.equal("John Doe");
+
+          done();
+        });
+    });
+  });
+});
