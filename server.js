c780a8cd9ff898f5c72b34027020fff584d61327
Viviño <Vivinh0@users.noreply.github.com>
2021-01-16 11:45:26 +0100

    Add express body parsers

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ require("dotenv").config();
 // Init db
 require("./config/db");
 
+// Setup middelwares
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-
 app.use("/api/exercise", require("./routers/userRouter"));
 
+// Start server
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
