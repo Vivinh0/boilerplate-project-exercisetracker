8be01f2383d19af7a4b20613579f3f37567080f6
Viviño <Vivinh0@users.noreply.github.com>
2021-01-16 12:43:56 +0100

    Add GET /api/exercise/users endpoint

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const userModel = require("../models/userModel");
+const usersModel = require("../models/usersModel");
 
 module.exports = {
   createUser: async (req, res, next) => {
     try {
       // Create user in db
       const userToCreate = { username: req.body.username };
-      const createdUser = await userModel.create(userToCreate);
+      const createdUser = await usersModel.create(userToCreate);
       // Create copy created user without __v property
       const response = {
         _id: createdUser._id,
@@ -17,6 +17,15 @@ module.exports = {
       res.status(201).send(response);
 
       // Catch any errors and send back 500 (Internal server error)
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const foundUsers = await usersModel.find({}, "_id username");
+      res.json(foundUsers);
     } catch (error) {
       console.log(error);
       res.sendStatus(500);
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/usersController");
+const usersController = require("../controllers/usersController");
 
 // module.exports = router.post("/new-user", (req, res, next) =>
 //   res.json({ msg: "Ok" })
 // );
 
-module.exports = router.post("/new-user", userController.createUser);
+module.exports = router
+  .get("/users", usersController.getUsers)
+  .post("/new-user", usersController.createUser);
