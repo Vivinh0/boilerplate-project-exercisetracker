3b280059430b8c0e8998e896c8ac6e4f13edc062
Viviño <Vivinh0@users.noreply.github.com>
2021-01-18 10:56:11 +0100

    Add log subschema

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,8 +3,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const logSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+  },
+  {
+    _id: false,
+  }
+);
+
 const userSchema = new Schema({
   username: { type: String, require: true },
+  log: { type: [logSchema] },
 });
 
 const userModel = mongoose.model("user", userSchema);
